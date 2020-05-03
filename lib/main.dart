4b19348f367823ad298fa48dc2448fe64ac36484
Daniel Gimbatschki <dgimb89@googlemail.com>
2020-05-03 13:37:24 +0200

    Refactor TodoList

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'todo.dart';
 import 'todo_form.dart';
-import 'todo_table.dart';
 
 void main() => runApp(OpenTODOs());
 
@@ -15,21 +14,38 @@ class OpenTODOs extends StatelessWidget {
         primaryColor: Colors.blue[900],
         accentColor: Colors.blue,
       ),
-      home: MyHomePage(title: 'TODOs'),
+      home: TodoList(title: 'TODOs'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class TodoList extends StatefulWidget {
+  TodoList({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _TodoListState createState() => _TodoListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _TodoListState extends State<TodoList> {
+  Future<List<Todo>> _futureTodos;
+
+  Future<List<Todo>> fetchTodos(String scopeId) {
+    setState(() { _futureTodos = fetchScope(scopeId); });
+    return _futureTodos;
+  }
+
+  Future<void> _handleRefresh() async {
+    await fetchTodos(Todo.SAMPLE_SCOPE_ID);
+    return null;
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    fetchTodos(Todo.SAMPLE_SCOPE_ID);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +54,18 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: TodoTable()
+        child: FutureBuilder<List<Todo>>(
+          future: _futureTodos,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            } else if (snapshot.hasData) {
+              return snapshot.data.length > 0
+                ? RefreshIndicator(child: ListView(children: _getTodos(snapshot.data)), onRefresh: _handleRefresh)
+                : _getNoElements();
+            }
+            return CircularProgressIndicator();
+          })
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -47,6 +74,26 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Create TODO',
         child: Icon(Icons.add),
       ),
+    );
+  }
+
+  List<Widget> _getTodos(List<Todo> todos) {
+    var widgets = List<Widget>();
+    todos.forEach((todo) {
+      widgets.add(ListTile(
+        leading: Icon(todo.getProgressIcon()),
+        title: Text(todo.name),
+        trailing: Icon(todo.getIcon()),
+      ));
+      widgets.add(Divider(color: Colors.blueGrey));
+    });
+    return widgets;
+  }
+
+  Widget _getNoElements() {
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Text('Nothing here... Create some TODOs!')
     );
   }
 }
